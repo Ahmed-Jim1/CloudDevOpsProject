a0b17a93a7ad14af9530645195f58de542200dd5
Ahmed Mahmoud <111567546+Ahmed-Jim1@users.noreply.github.com>
2024-12-26 02:36:18 +0200

    Update checkoutPipeline.groovy

--- shared-library/vars/checkoutPipeline.groovy
+++ shared-library/vars/checkoutPipeline.groovy
@@ -1,10 +1,5 @@
-def call(String repoUrl, String branch = 'main') {
-    stage('Git Checkout') {
-        steps {
-            script {
-                echo 'Cloning code from Git repository...'
-            }
-            git branch: branch, url: repoUrl
-        }
-    }
+// vars/gitCheckout.groovy
+def call(String repoUrl, String branch) {
+    echo "Cloning code from Git repository: ${repoUrl} (Branch: ${branch})"
+    git branch: branch, url: repoUrl
 }
